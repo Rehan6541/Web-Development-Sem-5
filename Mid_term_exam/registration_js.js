f4705f283de0c4fcec6588923bff22926fc115a2
Rehan Attar <147532439+Rehan6541@users.noreply.github.com>
2025-02-06 01:06:10 +0530

    Add files via upload

---
+++ Mid_term_exam/registration_js.js
@@ -0,0 +1,13 @@
+function reg(){
+var opwd=document.getElementById("password").value;
+var rpwd=document.getElementById("repassword").value;
+            {
+                if(opwd!=rpwd)
+                    alert("Passworrrd do not match")
+            }
+}
+
+
+
+
+
